65551bd917d0720e433a5c9a97fa8ea0e3b9c4e9
Ilan Grapel <igrapel@users.noreply.github.com>
2021-08-11 19:39:41 -0400

    Create scripts.js

---
+++ contact/scripts.js
@@ -0,0 +1,18 @@
+var w;
+var Move;
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var dist = $("#inputs").offset().left;
+    w = $(window).width();
+    w = w / 2;
+    w = w - 250;
+    move = "+=" + w;
+    console.log(move);
+    $("#inputs").animate(
+      {
+        marginLeft: w + "px"
+      },
+      1000
+    );
+  });
+});
