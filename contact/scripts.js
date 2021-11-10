167d5b92dc548dda86e2f8a2ee0093156b5c6465
Ilan Grapel <igrapel@users.noreply.github.com>
2021-11-10 14:40:45 -0500

    Update scripts.js

--- contact/scripts.js
+++ contact/scripts.js
@@ -1,13 +1,17 @@
 var w;
+var l;
 var Move;
 $(document).ready(function () {
   $(window).scroll(function () {
     var dist = $("#inputs").offset().left;
     w = $(window).width();
+    console.log("initial w: " + w);
+    l = $(letter).width();
+    console.log("initial l: " + l);
     w = w / 2;
-    w = w - 235;
+    w = w - l / 2 - w * 0.1;
     move = "+=" + w;
-    console.log(move);
+    console.log("move :" + move);
     $("#inputs").animate(
       {
         marginLeft: w + "px"
